4c7a17b7018a5445b9ea7653e64eee937b32b78b
CHelmstreit <chelmstreit@gmail.com>
2016-06-29 23:03:50 -0700

    fixed comment-outs

--- script.js
+++ script.js
@@ -1,190 +1,198 @@
 //STEP 1
-//var someMonth;
-//function theMonth(someMonth) {
-//    "use strict";
-//    return someMonth;
-//}
-//const currentMonth = June;
-//var summerMonth = ["June", "July", "August"];
-
-//STEP 2
-//var x = 10;
-//var x = "Toyota";
-//var isValid = true;
-//var Toyota = null; Toyota
-
-//STEP 3
-//var anExpression = 1 + (3 * 6) / 9;
-//var anotherExpression = anExpression + (3 * 10);
-
-//STEP 4
-//var varFirstName;
-//var varLastName;
-//var varAddress;
-//var varCity;
-//var varState;
-//var varZipCode;
-//var varYourAge;
-//var varReferralSource;
-//var varMayWeContactYou;
-
+/*
+var someMonth;
+function theMonth(someMonth) {
+        "use strict";
+    return someMonth;
+}
+const currentMonth = June;
+var summerMonth = ["June", "July", "August"];
+*/
+STEP 2
+/*
+var x = 10;
+var x = "Toyota";
+var isValid = true;
+var Toyota = null; Toyota
+*/
+STEP 3
+/*
+var anExpression = 1 + (3 * 6) / 9;
+var anotherExpression = anExpression + (3 * 10);
+*/
+STEP 4
+/*
+var varFirstName;
+var varLastName;
+var varAddress;
+var varCity;
+var varState;
+var varZipCode;
+var varYourAge;
+var varReferralSource;
+var varMayWeContactYou;
+*/
 //STEP 5
-
+/*
     //METHOD 1
-//var varFirstName;
-//var varLastName;
-//var varAddress;
-//var varCity;
-//var varState;
-//var varZipCode;
-//var varYourAge;
-//var varReferralSource;
-//var varMayWeContactYou;
+var varFirstName;
+var varLastName;
+var varAddress;
+var varCity;
+var varState;
+var varZipCode;
+var varYourAge;
+var varReferralSource;
+var varMayWeContactYou;
 
-//varFirstName = "Johnny";
-//varLastName = "Jeanshortz";
-//varAddress = "123 Thumbring Place";
-//varCity = "Mulletville";
-//varState = "FL";
-//varZipCode = "12345";
-//varYourAge = "100";
-//varReferralSource = "Crocs Outlet Store";
-//varMayWeContactYou = "Yes";
-
-    //METHOD 2
-//var varFirstName = "Johnny";
-//var varLastName = "Jeanshortz";
-//var varAddress = "123 Thumbring Place";
-//var varCity = "Mulletville";
-//var varState = "FL";
-//var varZipCode = "12345";
-//var varYourAge = "100";
-//var varReferralSource = "Crocs Outlet Store";
-//var varMayWeContactYou = "Yes";
-
-    //METHOD 3 
-//var varFirstName = "Johnny", varLastName = "Jeanshortz", varAddress = "123 Thumbring Place", varCity = "Mulletville", varState = "FL", varZipCode = "12345", varYourAge = "100", varReferralSource = "Crocs Outlet Store", varMayWeContactYou = "Yes";
+varFirstName = "Johnny";
+varLastName = "Jeanshortz";
+varAddress = "123 Thumbring Place";
+varCity = "Mulletville";
+varState = "FL";
+varZipCode = "12345";
+varYourAge = "100";
+varReferralSource = "Crocs Outlet Store";
+varMayWeContactYou = "Yes";
+*/
+//METHOD 2
+/*
+var varFirstName = "Johnny";
+var varLastName = "Jeanshortz";
+var varAddress = "123 Thumbring Place";
+var varCity = "Mulletville";
+var varState = "FL";
+var varZipCode = "12345";
+var varYourAge = "100";
+var varReferralSource = "Crocs Outlet Store";
+var varMayWeContactYou = "Yes";
+*/
+//METHOD 3 
+/*
+var varFirstName = "Johnny", varLastName = "Jeanshortz", varAddress = "123 Thumbring Place", varCity = "Mulletville", varState = "FL", varZipCode = "12345", varYourAge = "100", varReferralSource = "Crocs Outlet Store", varMayWeContactYou = "Yes";
 
 //STEP 6
+/*
+var x = 3;
+console.log("I have "+ x +" dogs.");
 
-//var x = 3;
-//console.log("I have "+ x +" dogs.");
-
-//var drinkingAge = 21;
-//console.log(drinkingAge >= 18);
-
+var drinkingAge = 21;
+console.log(drinkingAge >= 18);
+*/
 //STEP 7 
-
-//var pet = "dog";
-//console.log("My pet " + pet + " is a " + breed + ".");
-//var breed = "German Shepherd";
-
+/*
+var pet = "dog";
+console.log("My pet " + pet + " is a " + breed + ".");
+var breed = "German Shepherd";
+*/
 //STEP 8
-
-//var someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."';
-//console.log(someString);
-
+/*
+var someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."';
+console.log(someString);
+*/
 //STEP 9 
+/*
+var x = 10;
+x = null;
+console.log(x);
 
-//var x = 10;
-//x = null;
-//console.log(x);
-
-//var x;
-//console.log(x);
-
+var x;
+console.log(x);
+*/
 //STEP 10
-
-//var x = "Hello!";
-//var y = 10;
-//var xy = false;
-//var xyx = { rate: 4, years: 10 };
-//var xyxy;
-//console.log(typeof x);
-//console.log(typeof y);
-//console.log(typeof xy);
-//console.log(typeof xyx);
-//console.log(typeof xyxy);  
-
+/*
+var x = "Hello!";
+var y = 10;
+var xy = false;
+var xyx = { rate: 4, years: 10 };
+var xyxy;
+console.log(typeof x);
+console.log(typeof y);
+console.log(typeof xy);
+console.log(typeof xyx);
+console.log(typeof xyxy);  
+*/
 //STEP 11
+/*
+var x = "Hello Chris Helmstreit";
+var y = ", welcome to the JavaScript class!";
 
-//var x = "Hello Chris Helmstreit";
-//var y = ", welcome to the JavaScript class!";
-
-//alert(x + " " + y);
-
+alert(x + " " + y);
+*/
 //STEP 12
-
-//var name = "Chris";
-//alert("Hello " + name + ", welcome to the JavaScript class!");
-
+/*
+var name = "Chris";
+alert("Hello " + name + ", welcome to the JavaScript class!");
+*/
 //STEP 13
-//var name = "Chris";
-//var course = "JavaScript";
-//alert("Hello " + name + ", welcome to the " + course + " class!");
-
+/*
+var name = "Chris";
+var course = "JavaScript";
+alert("Hello " + name + ", welcome to the " + course + " class!");
+*/
 //STEP 14
-//var name = "Chris";
-//var course = "JavaScript";
-//alert("Hello " + name + ", \nwelcome to the " + course + " class!");
-
+/*
+var name = "Chris";
+var course = "JavaScript";
+alert("Hello " + name + ", \nwelcome to the " + course + " class!");
+*/
 //STEP 15
-
-//var name = prompt("Please enter your name.");
-//alert("Hello " + name + ", welcome to the JavaScript class!");
-
+/*
+var name = prompt("Please enter your name.");
+alert("Hello " + name + ", welcome to the JavaScript class!");
+*/
 //STEP 16
-
-//var name = prompt("Please enter your name.");
-//var course = prompt("What class are you taking?");
-//alert("Hello " + name + ", weclome to the " + course + " class!");
-
+/*
+var name = prompt("Please enter your name.");
+var course = prompt("What class are you taking?");
+alert("Hello " + name + ", weclome to the " + course + " class!");
+*/
 //STEP 17
-
-//var x = 10;
-//var y = 20;
-//console.log(x+y);
-
+/*
+var x = 10;
+var y = 20;
+console.log(x+y);
+*/
 //STEP 18
-
-//var x = 20;
-//console.log(x+20);
-
+/*
+var x = 20;
+console.log(x+20);
+*/
 //STEP 19
-//var x = 20;
-//console.log(x*5);
-
+/*
+var x = 20;
+console.log(x*5);
+*/
 //STEP 20
-//var x = (20%3);
-//console.log(x);
-
+/*
+var x = (20%3);
+console.log(x);
+*/
 //STEP 21
-//var x = 100;
-//var y = 100;
-//console.log(x==y);
-
+/*
+var x = 100;
+var y = 100;
+console.log(x==y);
+*/
 //STEP 22
-
-//var x = 50;
-//var y = 75;
-//console.log(x>=y);
-
+/*
+var x = 50;
+var y = 75;
+console.log(x>=y);
+*/
 //STEP 23
-
-//function Widget() {}
-//Widget1 = new Widget();
-//console.log(typeof Widget);
-
+/*
+function Widget() {}
+Widget1 = new Widget();
+console.log(typeof Widget);
+*/
 //STEP 24
-
-//var widget = [];
-//console.log(widget instanceof Object);
-
+/*
+var widget = [];
+console.log(widget instanceof Object);
+*/
 //STEP 25
-
-//var widget = [];
-//console.log(widget instanceof Date);
-
-
-
-
+/*
+var widget = [];
+console.log(widget instanceof Date);
+*/
